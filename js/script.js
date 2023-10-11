51101bce1669b0d19b11534886aa8785d20eccc7
sanjib3030 <sanjibsarkar3030@gmail.com>
2023-10-11 13:00:09 +0600

    second

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.carosol-main').slick({
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows:false,
+    autoplay:true,
+
+  
+  });
+
+
+})
